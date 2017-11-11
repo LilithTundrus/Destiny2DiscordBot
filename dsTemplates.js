82ce851691cbda87f3a3e2060008a7a220dccfe0
Lilith Tundrus <echofox123@gmail.com>
2017-11-11 16:27:58 -0500

    added dsTemplates.js class file

---
+++ dsTemplates.js
@@ -0,0 +1,16 @@
+//TODO: Add fields/footnote placeholders/etc.
+
+class baseDiscordEmbed {
+    constructor() {
+        this.author = {
+            name: '',
+            icon_url: ''
+        };
+        this.color = 3447003;
+        this.title = '';
+        this.description = '';
+    }
+}
+
+
+exports.baseDiscordEmbed = baseDiscordEmbed
