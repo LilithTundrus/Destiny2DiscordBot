27cdb6c747f3aed31372e1d25ac86c834e8b513b
Lilith Tundrus <echofox123@gmail.com>
2017-11-15 23:53:30 -0500

    added config-template.js

---
+++ config-template.js
@@ -0,0 +1,17 @@
+//token for discord bot access
+exports.discordToken = 'YOUR_DISCORD_TOKEN_HERE';
+exports.discordAdmin = 'ADMIN_DISCORD_ID';
+
+//bungie API token/info
+exports.destiny2Token = 'YOUR_D2_APP_TOKEN_HERE';
+exports.destiny2BaseURL = 'https://www.bungie.net';
+
+//D2 data for the bot
+
+// This will get dropped in the future for a database implimentation
+exports.destiny2ClanID = 'YOUR_CLAN_ID_HERE';
+
+//Icons/Image locations
+exports.travelerIcon = 'IMAGE_LOCATION_FOR_BOT';
+
+//put anything else sensitive here and upload as config template
