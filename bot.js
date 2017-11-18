e8ef98909f8a8e002343f1b9792667c53dfeb0a3
Lilith Tundrus <echofox123@gmail.com>
2017-11-17 22:45:40 -0500

    added a default switch case statement.

--- bot.js
+++ bot.js
@@ -138,6 +138,16 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 }
                 break;
             // Just add any case commands here
+            default:
+            var errMessageEmbed = new dsTemplates.baseDiscordEmbed;
+            errMessageEmbed.description = `${message} is not recognized as an innternal or external command. Type %help for help`;
+            errMessageEmbed.title = 'Error:';
+            bot.sendMessage({
+                to: channelID,
+                message: '',
+                embed: errMessageEmbed,
+                typing: true
+            });
         }
     }
 });
